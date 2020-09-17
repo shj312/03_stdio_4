12c860a9ca04fac619be284cbba0f3f46cf8ae9d
shj312 <sshj312@naver.com>
2020-09-17 16:43:03 +0900

    03_stdio_4

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a;
+	float b;
+	printf("input two integers :");
+	scanf("%d %f", &a, &b);
+	
+	printf("result is %f\n", a/b);
+	
+	return 0;
+}
